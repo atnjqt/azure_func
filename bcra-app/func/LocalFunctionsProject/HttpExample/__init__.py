946c0fb1675e16871ee54f2052d380921401482c
Etienne Jacquot <etienne.jacquot@asc.upenn.edu>
2021-05-13 12:02:52 -0400

    func dev for bcra calculation

--- bcra-app/func/LocalFunctionsProject/HttpExample/__init__.py
+++ bcra-app/func/LocalFunctionsProject/HttpExample/__init__.py
@@ -26,45 +26,47 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Prepare logging
         logging.info('Python HTTP trigger function processed a BCRA absolute_risk request.')
 
-        # Prepare DataFrame from HTTP request
+        # Prepare DataFrame from HTTP GET request
         try:
                 PID = int(req.params.get('PID'))
         except:
                 return func.HttpResponse(
-                        json.dumps({"ERROR_MSG":"PID failed for participant index..."}),
-                        status_code=400)
+                        json.dumps({"ERROR_MSG":"PID failed for participant index...",
+                                "ERROR_VAL":req.params.get('PID')}),
+                                status_code=400)
 
         try:
                 T1 = int(req.params.get('T1'))
         except:
                 return func.HttpResponse(
-                        json.dumps({"ERROR_MSG":"T1 failed for age... THIS IS A CICD TEST! - ATN & MARIO",
-                                    "ERROR_VAL":req.params.get('T1'),
-                                    "ATN_SAYS_ARF":'hello world!'}),
-                        status_code=400)
+                        json.dumps({"ERROR_MSG":"T1 failed for participant age...",
+                                "ERROR_VAL":req.params.get('T1')}),
+                                status_code=400)
         try:
-                #T2 = T1 + 10
                 T2 = int(req.params.get('T2'))
         except:
+                #T2 = T1 + 10 # <-- 10 year risk calculation, manually defined
                 return func.HttpResponse(
-                        json.dumps({"ERROR_MSG":"T2 failed for projected age..."}),
-                        status_code=400)
+                        json.dumps({"ERROR_MSG":"T2 failed for projected age...",
+                                "ERROR_VAL":req.params.get('T2')}),
+                                status_code=400)
 
         try:
                 N_Biop = int(req.params.get('N_Biop'))
         except:
                 return func.HttpResponse(
-                        json.dumps({"ERROR_MSG":"N_Biop failed for number of biops... THIS IS A CICD TEST!",
-                                    "ERROR_VAL":req.params.get('N_Biop')}),
-                        status_code=400)
+                        json.dumps({"ERROR_MSG":"N_Biop failed for biops...",
+                                "ERROR_VAL":req.params.get('N_Biop')}),
+                                status_code=400)
 
         try:
                 HypPlas = int(req.params.get('HypPlas'))
         except:
                 HypPlas = int('99') # <-- this we can just set as unknown if no val
                 #return func.HttpResponse(
-                #        json.dumps({"ERROR_MSG":"HypPlas failed for hyperplasia -- 99 unknown?..."}),
-                #        status_code=400)
+                #        json.dumps({"ERROR_MSG":"HypPlas failed for hyperplasia -- 99 unknown?...",
+                #               "ERROR_VAL":req.params.get('HypPlas')}),
+                #               status_code=400)
 
         try:
                 AgeMen = int(req.params.get('AgeMen'))
@@ -72,30 +74,34 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 # maybe we need a dict which matches the qualtrics categorical responses to num?
                 # or flags in BCRA for this? not sure ... 
                 return func.HttpResponse(
-                        json.dumps({"ERROR_MSG":"AgeMen failed... is this categorical or numeric?"}),
-                        status_code=400)
+                        json.dumps({"ERROR_MSG":"AgeMen failed... is this categorical or numeric?",
+                                "ERROR_VAL":req.params.get('AgeMen')}),
+                                status_code=400)
         try:
                 Age1st = int(req.params.get('Age1st'))
         except:
                 return func.HttpResponse(
-                        json.dumps({"ERROR_MSG":"Age1st failed for age of first child..."}),
-                        status_code=400)
+                        json.dumps({"ERROR_MSG":"Age1st failed... is this categorical or numeric?",
+                                "ERROR_VAL":req.params.get('Age1st')}),
+                                status_code=400)
         try:                
                 N_Rels = int(req.params.get('N_Rels'))
         except:
                 return func.HttpResponse(
-                json.dumps({"ERROR_MSG":"N_Rels failed for number of relatives w/ BCRA..."}),
-                status_code=400)
+                        json.dumps({"ERROR_MSG":"N_Rels failed for number of relatives w/ BCRA...",
+                                "ERROR_VAL":req.params.get('Age1st')}),
+                                status_code=400)
         try:        
                 Race = int(req.params.get('Race'))
         except:
                 # maybe we need a dict which matches the qualtrics categorical response to numeric val for race
                 return func.HttpResponse(
-                        json.dumps({"ERROR_MSG":"Race failed for val..."}),
-                        status_code=400)
+                        json.dumps({"ERROR_MSG":"Race failed for val...",
+                                "ERROR_VAL":req.params.get('Race')}),
+                                status_code=400)
 
 
-        logging.info('http get request syntax ok...')
+        logging.info('HTTP get request syntax ok...')
 
         # Create dataframe from HTTP get values:
         df = pd.DataFrame({'id':PID,
@@ -121,14 +127,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         #check_summary = bcra.check_summary
         absolute_risk = bcra.absolute_risk
         #error_check = bcra.error_check
-        #relative_risk = bcra.relative_risk
+        relative_risk = bcra.relative_risk
 
         # Convert to R dataframe
         pandas2ri.activate()
         r_dt = ro.conversion.py2rpy(df) # df is a pd.DataFrame object
 
+        logging.info('Calculating absolute risk (DEV) ...')
+
         # Return check summary bcra result
         # Qualtrics embedded data is json structure
         return func.HttpResponse(
-                json.dumps({"absolute_risk":absolute_risk(r_dt).tolist()[0]}),
+                json.dumps({"absolute_risk":absolute_risk(r_dt).tolist()[0],
+                            "relative_risk":relative_risk(r_dt).to_dict()}),
                 status_code=200)
